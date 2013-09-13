537a18ce763c002e39fa0db18e570f50a23fba2f
unknown <sobek7@gmail.com>
2013-09-12 23:04:47 -0400

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+JoyMirth::Application.config.secret_key_base = '54979bb6b0226a27e9da0aa8d0e1d167a6603ad612f19e4e26f3c41246628f6a70901e88e0e82b121326e97590533a96e57d000e9a77fa3366f2622c8bae3d1d'
